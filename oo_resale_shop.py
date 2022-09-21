dcac27004bbffb1dfccb23d14dd586695e048499
Arogya Saakha <aros22@hampshire.edu>
2022-09-20 23:56:35 -0400

    Done

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -4,7 +4,6 @@ class ResaleShop:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
-    # What methods will you need?
+    def __init__(self):
+        pass
+        # What methods will you need?
